1d24d9f2ce49a23e0ca8df2cf3af42a1b49d39f8
Saad Khan <sk6786@g.rit.edu>
2020-09-24 16:48:27 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
   }
 
   callAPI() {
-    fetch("http://localhost:9000/testAPI")
+    fetch(process.env.APP_SERVER_URL + "/testAPI")
         .then(res => res.text())
         .then(res => this.setState({apiResponse: res}));
   }
