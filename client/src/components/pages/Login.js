da75afa814337305953b774da08790e73746e96e
Jordan Bowman <moreemothansasuke@gmail.com>
2020-10-01 23:18:40 -0400

    Improve login ui and save entered values

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,10 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { Link } from "react-router-dom"
 import { connect } from 'react-redux';
 import { updateUser, changeCurrentPage } from '../../redux/Actions'
 import './Login.css'
+import { Grid } from '@material-ui/core';
 
 function Login(props) {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
 
     useEffect(() => {
         if (props.user !== undefined && Object.keys(props.user).length > 0) {
@@ -14,10 +19,10 @@ function Login(props) {
     })
 
     function attemptLogin() {
-        props.updateUser({ username: "temp", pw: "more temp" })
+        props.updateUser({ username, password })
         props.history.push('/Home')
 
-        // Leaving for now since we will eventually need api calls, will be remove before merge with sprint branch
+        // Leaving for now since we will eventually need api calls, will be removed before merge with sprint branch
         /*fetch("http://localhost:9000/login")
             .then(response => response.json())
             .then(response => {
@@ -31,17 +36,42 @@ function Login(props) {
             });*/
     }
 
+    function handleChange(event) {
+        switch (event.target.name) {
+            case "username":
+                setUsername(event.target.value)
+                break;
+            case "password":
+                setPassword(event.target.value)
+                break;
+        }
+
+    }
+
     return (
         <div className="home">
             <div className="loginContainer">
-                <div className="loginHeading">Login:</div>
-                <form name="login" className="loginForm">
-                    <label htmlFor="username" className="loginLabel">Username:</label>
-                    <input name="username" type="text" className="loginInput"></input>
-                    <label htmlFor="password" className="loginLabel">Password:</label>
-                    <input name="password" type="password" className="loginInput"></input>
-                    <Button variant="contained" color="inherit" className="loginSubmit" onClick={attemptLogin}>Submit</Button>
-                </form>
+                <div className="loginHeading">Log In:</div>
+                <Grid container alignItems="center" justify="center" spacing={4}>
+                    <Grid item xs={6}>
+                        <label htmlFor="username" className="loginLabel">Username:</label>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <TextField name="username" id="username" className="loginInput" value={username} onChange={handleChange} />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <label htmlFor="password" className="loginLabel">Password:</label>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <TextField name="password" id="password" type="password" className="loginInput" value={password} onChange={handleChange} />
+                    </Grid>
+                    <Grid item xs={12} align="center">
+                        <Button variant="contained" color="inherit" className="loginSubmit" onClick={attemptLogin}>Submit</Button>
+                    </Grid>
+                    <Grid item xs={12} align="center">
+                        <div>Don't have an account? Sign up <Link to="/SignUp" onClick={() => { console.log("click") }}>here</Link>!</div>
+                    </Grid>
+                </Grid>
             </div>
         </div>
     );
