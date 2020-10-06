d0315983ef84d8c996a80da7894eec78f9ba3cba
Dung <dxt6193@rit.edu>
2020-10-06 20:31:17 +0700

    Set up sign up UI

--- client/src/components/pages/SignUp.js
+++ client/src/components/pages/SignUp.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { changeCurrentPage } from '../../redux/Actions'
+import {Container, Box, Button, Grid, Typography, TextField} from '@material-ui/core';
+import './SignUp.css';
 
 function SignUp(props) {
     useEffect(() => {
@@ -11,9 +13,32 @@ function SignUp(props) {
     });
 
     return (
-        <div>
-            Sign Up - TODO
-        </div>
+        <Container>
+            <Box className={'signUpContainer'}>
+                <Typography className={'formRow'} align={'center'} variant={'h6'}>Sign Up</Typography>
+                <Grid container justify={'center'} alignItems={'center'}>
+                    <Grid className={'formRow'} container justify={'center'} alignItems={'center'} spacing={3}>
+                        <Grid item>
+                            <Typography variant={'body1'}>Username: </Typography>
+                        </Grid>
+                        <Grid item>
+                            <TextField name={'username'} id={'username'} />
+                        </Grid>
+                    </Grid>
+                    <Grid className={'formRow'} container justify={'center'} alignItems={'center'} spacing={3}>
+                        <Grid item>
+                            <Typography variant={'body1'}>Password: </Typography>
+                        </Grid>
+                        <Grid item>
+                            <TextField name={'password'} id={'password'} />
+                        </Grid>
+                    </Grid>
+                    <Grid className={'formRow'} item justify={'center'}>
+                        <Button color={'primary'} variant={'contained'}>Sign Up</Button>
+                    </Grid>
+                </Grid>
+            </Box>
+        </Container>
     );
 }
 
