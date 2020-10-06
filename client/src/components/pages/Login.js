a4c5be7d5164fc555af3966971b1624214b4c937
Jordan Bowman <moreemothansasuke@gmail.com>
2020-10-06 17:44:05 -0400

    Add materialui lab and add error message

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import Alert from '@material-ui/lab/Alert';
 import { Link } from "react-router-dom"
 import { connect } from 'react-redux';
 import { updateUser, changeCurrentPage } from '../../redux/Actions'
@@ -10,6 +12,7 @@ import { Grid } from '@material-ui/core';
 function Login(props) {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
+    const [errorHappened, setErrorHappened] = useState(false)
 
     useEffect(() => {
         if (props.user !== undefined && Object.keys(props.user).length > 0) {
@@ -19,21 +22,24 @@ function Login(props) {
     })
 
     function attemptLogin() {
-        props.updateUser({ username, password })
-        props.history.push('/Home')
-
-        // Leaving for now since we will eventually need api calls, will be removed before merge with sprint branch
-        /*fetch("http://localhost:9000/login")
-            .then(response => response.json())
-            .then(response => {
-                if (response.user !== undefined) {
-                    props.updateUser(response.user)
-                    props.history.push('/')
-                }
-                else {
-                    // show that attempt to login was unsuccessful
-                }
-            });*/
+        /*if (username === "") {
+            setErrorHappened(true);
+        } else {
+            props.updateUser({ username: "hello", other: "other" })
+            props.history.push('/Home')
+        }*/
+        let url = process.env.REACT_APP_ENVIRONMENT === 'prod' ? process.env.REACT_APP_PROD_SERVER_URL : process.env.REACT_APP_DEV_SERVER_URL;
+        axios.post(url + "/Login", { username, password }).then(response => {
+            if (response.status === 401) {
+                setErrorHappened(true);
+            } else {
+                props.updateUser(response.user)
+                props.history.push('/Home')
+            }
+        }).catch(response => {
+            setErrorHappened(true);
+            console.log(response)
+        })
     }
 
     function handleChange(event) {
@@ -45,7 +51,6 @@ function Login(props) {
                 setPassword(event.target.value)
                 break;
         }
-
     }
 
     return (
@@ -53,6 +58,11 @@ function Login(props) {
             <div className="loginContainer">
                 <div className="loginHeading">Log In:</div>
                 <Grid container alignItems="center" justify="center" spacing={4}>
+                    {errorHappened &&
+                        <Grid item xs={12} align="center">
+                            <Alert severity="error">Your Username and/or Password was incorrect, please try again.</Alert>
+                        </Grid>
+                    }
                     <Grid item xs={6}>
                         <label htmlFor="username" className="loginLabel">Username:</label>
                     </Grid>
