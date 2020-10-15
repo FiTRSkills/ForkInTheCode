70735d45f74a1660d828a2ad1930926bac378320
RachaelDaly <31971164+RachaelDaly@users.noreply.github.com>
2020-10-15 14:14:07 -0400

    Update commands.js
    removed commented out example code

--- client/cypress/support/commands.js
+++ client/cypress/support/commands.js
@@ -1,15 +1,5 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
+
+// -- Login  command --
 Cypress.Commands.add("fakeLogin", () => {
     cy.visit('/Login');
     cy.server()
@@ -26,15 +16,3 @@ Cypress.Commands.add("fakeLogin", () => {
     cy.get('#submit').click()
     cy.wait('@loginCall')
 })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
