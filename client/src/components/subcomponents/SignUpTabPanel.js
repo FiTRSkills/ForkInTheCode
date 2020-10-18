be831bb63d5c1b97fee204b2bdd557b2ea150321
Saad Khan <sk6786@g.rit.edu>
2020-10-18 13:18:19 -0400

    Toni's jumpy UI fix

--- client/src/components/subcomponents/SignUpTabPanel.js
+++ client/src/components/subcomponents/SignUpTabPanel.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
-import {Box, Button, TextField, Typography} from "@material-ui/core";
+import { Box, Button, TextField, Typography } from "@material-ui/core";
 import "./SignUpTabPanel.css";
 import axios from "axios";
 import { Alert } from "@material-ui/lab";
-import {makeStyles} from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 
 const url =
-  process.env.REACT_APP_ENVIRONMENT === "prod"
-    ? process.env.REACT_APP_PROD_SERVER_URL
-    : process.env.REACT_APP_DEV_SERVER_URL;
+    process.env.REACT_APP_ENVIRONMENT === "prod"
+        ? process.env.REACT_APP_PROD_SERVER_URL
+        : process.env.REACT_APP_DEV_SERVER_URL;
 
 function SignUpTabPanel(props) {
   const { currentTabIdx, index, title, usertype, history, form_id } = props;
@@ -35,25 +35,25 @@ function SignUpTabPanel(props) {
     event.preventDefault();
     setLoading(true);
     axios
-      .post(url + "/register", {
-        usertype,
-        username,
-        password,
-      })
-      .then((response) => {
-        if (response.data.name && response.data.name === "UserExistsError") {
-          setError(response.data.message);
-        } else {
-          history.push("/Login");
-        }
-      })
-      .catch((error) => {
-        setError("Something wrong occurs!");
-        console.log(error);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+        .post(url + "/register", {
+          usertype,
+          username,
+          password,
+        })
+        .then((response) => {
+          if (response.data.name && response.data.name === "UserExistsError") {
+            setError(response.data.message);
+          } else {
+            history.push("/Login");
+          }
+        })
+        .catch((error) => {
+          setError("Something wrong occurs!");
+          console.log(error);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
   };
   const useStyles = makeStyles((theme) => ({
     paper: {
@@ -67,63 +67,67 @@ function SignUpTabPanel(props) {
       backgroundColor: theme.palette.secondary.main,
     },
     form: {
-      width: "100%",
+      maxWidth: 480,
       marginTop: theme.spacing(1),
     },
     submit: {
       margin: theme.spacing(3, 0, 2),
-      width:"100%"
+      width: "100%",
     },
     header: {
       marginTop: theme.spacing(2),
-      marginBottom: theme.spacing(1)
-    }
+      marginBottom: theme.spacing(1),
+    },
   }));
   const classes = useStyles();
 
   return (
+      <Box hidden={currentTabIdx !== index}>
+        <Typography
+            component="h1"
+            variant="h5"
+            align={"center"}
+            className={classes.header}
+        >
+          Sign Up for {title}
+        </Typography>
 
-      <Box
-      component={"form"}
-      hidden={currentTabIdx !== index}
-      onSubmit={signUp}
-      id={form_id}
-    >
-      <Typography component="h1" variant="h5" align={"center"} className = {classes.header}>
-        Sign Up for {title}
-      </Typography>
-
-      {error && <Alert severity={"error"}>{error}</Alert>}
-      <form className={classes.form}>
-        <TextField
-            variant="outlined"
-            margin="normal"
-            required={true}
-            fullWidth
-            id="username"
-            label="Username"
-            name="username"
-            autoFocus
-            value={username}
-            onChange={handleChange}
-        />
-        <TextField
-            variant="outlined"
-            margin="normal"
-            required={true}
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            value={password}
-            onChange={handleChange}
-        />
-          <Button color={"primary"} variant={"contained"} type={"submit"} className={classes.submit}>
+        {error && <Alert severity={"error"}>{error}</Alert>}
+        <form className={classes.form} onSubmit={signUp} id={form_id}>
+          <TextField
+              variant="outlined"
+              margin="normal"
+              required={true}
+              fullWidth
+              id="username"
+              label="Username"
+              name="username"
+              autoFocus
+              value={username}
+              onChange={handleChange}
+          />
+          <TextField
+              variant="outlined"
+              margin="normal"
+              required={true}
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              value={password}
+              onChange={handleChange}
+          />
+          <Button
+              color={"primary"}
+              variant={"contained"}
+              type={"submit"}
+              className={classes.submit}
+          >
             {!loading ? "Sign Up" : "Processing..."}
           </Button>
-      </form>
-    </Box>
+        </form>
+      </Box>
   );
 }
 
