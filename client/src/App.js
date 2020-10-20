4b8a7211bd652a2691f70289459b646b57aee4a8
Saad Khan <sk6786@g.rit.edu>
2020-10-19 21:23:45 -0400

    commit for dep

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import Home from './components/pages/Home'
 import Login from './components/pages/Login'
