1da3b14747a607d95349acb00b168497dfe9a52d
Alex <adw4236@rit.edu>
2020-10-23 16:06:00 -0400

    Added some comments to methods in models

--- server/src/models/educatorProfile.js
+++ server/src/models/educatorProfile.js
@@ -13,10 +13,23 @@ const EducatorProfile = new mongoose.Schema({
   },
 });
 
+/**
+ * Automatically populates various fields when finding the document so that
+ * information can be accessed more directly.
+ *
+ * @param id The id of the document to search for
+ * @returns {Promise<EducatorProfile>}
+ */
 EducatorProfile.statics.findAndPopulateById = function (id) {
   return this.findById(id).populate("organization").exec();
 };
 
+/**
+ * Sets the organization or creates a new one if the name is not found.
+ *
+ * @param name The name of the organization to match or create
+ * @returns {Promise<EducatorProfile>}
+ */
 EducatorProfile.methods.setOrganization = async function (name) {
   let org = await Organization.findOneOrCreate(name);
   this.organization = org._id;
