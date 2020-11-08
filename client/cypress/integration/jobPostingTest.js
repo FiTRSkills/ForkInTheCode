4ab2d9f66af46a6656a763b94e5dc0bacfcd3278
Dung <dxt6193@rit.edu>
2020-11-08 14:48:45 +0700

    Job posting mocked and test

---
+++ client/cypress/integration/jobPostingTest.js
@@ -0,0 +1,48 @@
+describe("Job Posting", () => {
+  it("Load job posting success", () => {
+    // Stub edit profile success response
+    cy.server();
+    cy.route({
+      method: "POST",
+      url: Cypress.env("REACT_APP_SERVER_URL") + "/jobposting",
+      status: 200,
+      response: {
+        organization: {
+          name: "Amazon",
+          location: "USA",
+          contact: {
+            address: "Silicon Valley",
+            email: "amazon@gmail.com",
+            phone: "0111222333",
+          },
+        },
+        jobTitle: "Software Engineer",
+        pay: "$12000",
+        code: 14623,
+        description: "Very difficult job",
+        qualifications: "Good coder",
+      },
+    }).as("jobPostingCall");
+
+    // Verify load job posting success
+    cy.visit(Cypress.env("REACT_APP_CLIENT_URL") + "/Job/30");
+    cy.wait("@jobPostingCall").its("status").should("eq", 200);
+    cy.contains("Good coder");
+  });
+
+  it("Load job posting failure", () => {
+    // Stub edit profile success response
+    cy.server();
+    cy.route({
+      method: "POST",
+      url: Cypress.env("REACT_APP_SERVER_URL") + "/jobposting",
+      status: 400,
+      response: "Bad request",
+    }).as("jobPostingCall");
+
+    // Verify load job posting success
+    cy.visit(Cypress.env("REACT_APP_CLIENT_URL") + "/Job/30");
+    cy.wait("@jobPostingCall").its("status").should("eq", 400);
+    cy.contains("Bad request");
+  });
+});
--- client/src/components/pages/JobPosting.js
+++ client/src/components/pages/JobPosting.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { changeCurrentPage } from "../../redux/actions";
 import Container from "@material-ui/core/Container";
@@ -8,19 +8,27 @@ import Button from "@material-ui/core/Button";
 import Box from "@material-ui/core/Box";
 import { makeStyles } from "@material-ui/core/styles";
 import JobPostingInfoItem from "../subcomponents/JobPostingInfoItem";
+import axios from "axios";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { Alert } from "@material-ui/lab";
 
 const mockedPosting = {
-  company: "Amazon",
-  pay_range: "$99,172 to $128,920 per year",
-  requirements:
-    "This position is open until filled and resumes will be reviewed every two weeks. The first batch of applicants will " +
-    "be reviewed on August 27, 2019.",
-  objectives:
-    "The IT Division is responsible for the planning, development and management of the IT systems and subsystems that " +
-    "support DC Superior Court case flow, office automation, special programs and management operations. The incumbent " +
-    "will serve as the specialist in planning, analyzing, designing, developing, and implementing software applications," +
-    " primarily in the areas of custom system development, database development, web development, and systems integration " +
-    "and interfacing.",
+  organization: {
+    name: "Amazon",
+    location: "USA",
+    contact: {
+      address: "Silicon Valley",
+      email: "amazon@gmail.com",
+      phone: "0111222333",
+    },
+  },
+  jobTitle: "Software Engineer",
+  pay: "$12000",
+  code: 14623,
+  description:
+    "The IT Division is responsible for the planning, development and management of the IT systems and subsystems that support DC Superior Court case flow, office automation, special programs and management operations. The incumbent will serve as the specialist in planning, analyzing, designing, developing, and implementing software applications, primarily in the areas of custom system development, database development, web development, and systems integration and interfacing.",
+  qualifications:
+    "This position is open until filled and resumes will be reviewed every two weeks. The first batch of applicants will be reviewed on August 27, 2019.",
   skills: [{ name: "Java" }, { name: "Python" }, { name: "SQL" }],
 };
 
@@ -43,6 +51,15 @@ function JobPosting(props) {
    */
   const { id } = useParams();
 
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [jobTitle, setJob] = useState("");
+  const [pay, setPay] = useState("");
+  const [description, setDescription] = useState("");
+  const [qualifications, setQualifications] = useState("");
+  const [organization, setOrganization] = useState({});
+  const [code, setCode] = useState("");
+
   /**
    * Change the nav title to Job
    */
@@ -50,42 +67,81 @@ function JobPosting(props) {
     props.changeCurrentPage("Job");
   });
 
+  useEffect(() => {
+    loadJobPosting();
+  }, []);
+
+  function loadJobPosting() {
+    setLoading(true);
+    axios
+      .post(process.env.REACT_APP_SERVER_URL + "/jobposting", {
+        id,
+      })
+      .then((res) => {
+        // setOrganization(mockedPosting.organization);
+        // setJob(mockedPosting.jobTitle);
+        // setPay(mockedPosting.pay);
+        // setCode(mockedPosting.code);
+        // setDescription(mockedPosting.description);
+        // setQualifications(mockedPosting.qualifications);
+        setOrganization(res.data.organization);
+        setJob(res.data.jobTitle);
+        setPay(res.data.pay);
+        setCode(res.data.code);
+        setDescription(res.data.description);
+        setQualifications(res.data.qualifications);
+      })
+      .catch((error) => {
+        if (error.response.status === 400) {
+          setError(error.response.data);
+        } else if (error.response.status === 404) {
+          setError("Not found");
+        } else {
+          setError("Failed to load profile");
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
+
   const classes = useStyles();
 
   return (
     <Container>
-      <Box className={classes.jobHeader}>
-        <Typography variant={"h4"}>IT Specialist (APPSW)</Typography>
-        <Button name={"applyJob"} variant="contained" color="primary">
-          Apply Job
-        </Button>
-      </Box>
-      <JobPostingInfoItem
-        title={"Company"}
-        description={mockedPosting.company}
-      />
-      <JobPostingInfoItem
-        title={"Pay Range"}
-        description={mockedPosting.pay_range}
-      />
-      <JobPostingInfoItem
-        title={"Requirements"}
-        description={mockedPosting.requirements}
-      />
-      <JobPostingInfoItem
-        title={"Objectives"}
-        description={mockedPosting.objectives}
-      />
-      <JobPostingInfoItem
-        title={"Skills Needed to Apply"}
-        skills={mockedPosting.skills}
-        isSkills={true}
-      />
-      <JobPostingInfoItem
-        title={"Classes Available In Your Area"}
-        availableClasses={mockedClasses}
-        isClasses={true}
-      />
+      {error && <Alert severity={"error"}>{error}</Alert>}
+      {loading ? (
+        <CircularProgress />
+      ) : (
+        <Box>
+          <Box className={classes.jobHeader}>
+            <Typography variant={"h4"}>{jobTitle}</Typography>
+            <Button name={"applyJob"} variant="contained" color="primary">
+              Apply Job
+            </Button>
+          </Box>
+          <JobPostingInfoItem
+            title={"Company"}
+            description={organization.name}
+          />
+          <JobPostingInfoItem title={"Pay Range"} description={pay} />
+          <JobPostingInfoItem
+            title={"Requirements"}
+            description={qualifications}
+          />
+          <JobPostingInfoItem title={"Objectives"} description={description} />
+          <JobPostingInfoItem
+            title={"Skills Needed to Apply"}
+            skills={[]}
+            isSkills={true}
+          />
+          <JobPostingInfoItem
+            title={"Classes Available In Your Area"}
+            availableClasses={[]}
+            isClasses={true}
+          />
+        </Box>
+      )}
     </Container>
   );
 }
