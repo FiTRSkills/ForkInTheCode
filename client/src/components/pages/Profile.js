6ebeb27108c7fdea2cfd6f19dcc9d2191547dac7
Jordan Bowman <moreemothansasuke@gmail.com>
2020-11-11 16:16:45 -0500

    Remove warnings

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -3,12 +3,10 @@ import Container from "@material-ui/core/Container";
 import { changeCurrentPage } from "../../redux/actions";
 import { connect } from "react-redux";
 import MainProfile from "../subcomponents/MainProfile";
-import axios from "axios";
 import Divider from "@material-ui/core/Divider";
 import CareerItemList from "../subcomponents/CareerItemList";
 import EducationItemList from "../subcomponents/EducationItemList";
 import { makeStyles } from "@material-ui/core/styles";
-import { Alert } from "@material-ui/lab";
 
 const useStyles = makeStyles((theme) => ({
   container: {
