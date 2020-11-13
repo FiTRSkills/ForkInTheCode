f22257023e21dce44108104a7228883f067705b2
RachaelDaly <rld5989@g.rit.edu>
2020-11-13 16:42:10 -0500

    Updated job search and get job posting to not require session

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -231,7 +231,7 @@ router.post(
  * @property {string} id -  the job posting id
  * @returns {string} message - success message
  */
-router.get("/jobs/jobposting", sessionValidation, job.getJobPosting);
+router.get("/jobs/jobposting", job.getJobPosting);
 
 /**
  * Routing serving retrieving a job posting by id
@@ -268,7 +268,6 @@ router.post(
  */
 router.post(
   "/JobSearch",
-  sessionValidation,
   [
     check("zipCode", "Must send a viable zipcode").not().isEmpty(),
     check("skills", "Must send a viable skills list").optional({
