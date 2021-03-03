42fa95e31bee1d19b2c90568f92c4e56ccc9d26f
Alex <adw4236@rit.edu>
2021-03-03 02:58:23 -0500

    Fixed issues in jobPosting test after reverting some changes.

--- server/test/modelstest/jobPosting.test.js
+++ server/test/modelstest/jobPosting.test.js
@@ -170,7 +170,7 @@ describe("JobPosting Model Test", () => {
   it("add skills - single skill", async () => {
     let skills = await makeSkills();
     let jobPosting = new JobPosting();
-    await jobPosting.addSkills(skills[0]._id.toString());
+    await jobPosting.addSkills(["S1"]);
 
     let savedJobPosting = await JobPosting.getJobPosting(jobPosting._id);
     expect(savedJobPosting.skills.length).toEqual(1);
@@ -182,14 +182,12 @@ describe("JobPosting Model Test", () => {
   it("add skills - multiple skills", async () => {
     let skills = await makeSkills();
     let jobPosting = new JobPosting();
-    await jobPosting.addSkills([
-      skills[0]._id.toString(),
-      skills[1]._id.toString(),
-    ]);
+    await jobPosting.addSkills(["S1", "S4"]);
 
     let savedJobPosting = await JobPosting.getJobPosting(jobPosting._id);
     expect(savedJobPosting.skills.length).toEqual(2);
     expect(savedJobPosting.skills[0]._id).toEqual(skills[0]._id);
-    expect(savedJobPosting.skills[1]._id).toEqual(skills[1]._id);
+    let savedSkills = await Skill.find({}).exec();
+    expect(savedSkills.length).toEqual(4);
   });
 });
