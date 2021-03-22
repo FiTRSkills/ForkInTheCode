8cf52d0d452ce89ca43a221a07e99d372f1d91d5
Saad Khan <sk6786@g.rit.edu>
2021-03-22 16:25:47 -0400

    Revert "committing add edit"
    This reverts commit 054bb392

--- client/src/components/pages/AddEditCourse.js
+++ client/src/components/pages/AddEditCourse.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
-import { changeCurrentPage } from "../../redux/actions";
+import { changeCurrentPage, setCourseSuccessMessage } from "../../redux/actions";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
@@ -53,9 +53,7 @@ function AddEditCourse(props) {
   const [skillStrs, setSkillStrs] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [message, setMessage] = useState(null);
   const [authenticated, setAuthenticated] = useState(false);
-
   const { mode } = useParams();
 
   // Style hook
@@ -158,7 +156,6 @@ function AddEditCourse(props) {
   function onSubmit(event) {
     if (skillObjects.length === 0) {
       setError("Skills are required");
-      setMessage(null);
     } else {
       setLoading(true);
       switch (mode) {
@@ -202,8 +199,6 @@ function AddEditCourse(props) {
               } else {
                 setError("Failed to add course");
               }
-              setMessage(null);
-              console.log(error);
             })
             .finally(() => {
               setLoading(false);
@@ -250,8 +245,6 @@ function AddEditCourse(props) {
               } else {
                 setError("Failed to update course");
               }
-              setMessage(null);
-              console.log(error);
             })
             .finally(() => {
               setLoading(false);
@@ -271,7 +264,6 @@ function AddEditCourse(props) {
   return (
     <Container className={classes.container}>
       <Box className={classes.subContainer}>
-        {message && <Alert severity="success">{message}</Alert>}
         {error && <Alert severity="error">{error}</Alert>}
         <form onSubmit={onSubmit}>
           <Box className={classes.field}>
@@ -421,7 +413,7 @@ function AddEditCourse(props) {
               value={requiredEquipment}
               onChange={handleChange}
               multiline
-              row={4}
+              rows={4}
             />
           </Box>
           <Box className={classes.field}>
@@ -493,6 +485,7 @@ function mapStateToProps(state) {
 function mapDispatchToProps(dispatch) {
   return {
     changeCurrentPage: (content) => dispatch(changeCurrentPage(content)),
+    setCourseSuccessMessage: (content) => dispatch(setCourseSuccessMessage(content))
   };
 }
 
