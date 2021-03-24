4b10a55d1ed77b1341008a69653b28ac964f8ffe
Alex <adw4236@rit.edu>
2021-03-24 03:43:03 -0400

    Implemented subdomain for the api server

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const subdomain = require("express-subdomain");
 const path = require("path");
 
 const app = express();
 const api = require("./api");
 
-app.use(express.static(path.join(__dirname, "../../client/build")));
-app.use(api);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../client/build")));
+  console.log("PROD");
+  app.use(subdomain("api", api));
+} else {
+  console.log("DEV");
+  app.use(api);
+}
 
 module.exports = app;
