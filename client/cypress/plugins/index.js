b778f6be252fc2e89008d9e1f0016255b98724be
Saad Khan <sk6786@g.rit.edu>
2021-03-25 17:09:55 -0400

    idex file

--- client/cypress/plugins/index.js
+++ client/cypress/plugins/index.js
@@ -11,11 +11,17 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
-
+// const db = require("../../server/src/db");
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
+  on("task",{
+    'adduser':()=>{
+      return db.seed()
+    }
+
+  })
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
